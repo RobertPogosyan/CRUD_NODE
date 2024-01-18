1ea7421a5e367f7a20672eb61f09c517ee910e92
TatevMargaryan0331 <tatev.margaryan1.y@tumo.org>
2024-01-18 21:12:20 +0400

    commit

---
+++ public/script.js
@@ -0,0 +1,16 @@
+let age = document.getElementById("age");
+let name = document.getElementById("text");
+
+function sendData (){
+let usage = age.value;
+let usname = name.value
+
+ fetch("http://localhost:3000//addName", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  
+    body: JSON.stringify(age = usage , name = usname), // body data type must match "Content-Type" header
+});
+}
